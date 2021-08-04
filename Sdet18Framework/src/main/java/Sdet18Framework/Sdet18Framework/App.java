cbadb02f108c491c7270b6a1ed85a1ed73fa16f2
my <my@my-PC>
2021-08-04 13:42:03 +0530

    Hi this is my 1st jenkins commit

---
+++ Sdet18Framework/src/main/java/Sdet18Framework/Sdet18Framework/App.java
@@ -0,0 +1,13 @@
+package Sdet18Framework.Sdet18Framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ Sdet18Framework/src/test/java/com/crm/sde18/DemoTest.java
@@ -0,0 +1,10 @@
+package com.crm.sde18;
+
+import org.testng.annotations.Test;
+
+public class DemoTest {
+@Test
+public void demo()
+{
+	System.out.println("Demo");}
+}
